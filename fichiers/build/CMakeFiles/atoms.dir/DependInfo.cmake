9c5d8f301baca6385c10b7bdb0b0bb2558979d25
Alexis Couture <acouture@emi.u-bordeaux1.fr>
2015-03-04 14:19:59 +0100

    Ajout du dossier build

---
+++ fichiers/build/CMakeFiles/atoms.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/src/main.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/CMakeFiles/atoms.dir/src/main.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/src/tools.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/CMakeFiles/atoms.dir/src/tools.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "USE_DOUBLE=0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libsotl/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ fichiers/build/libsotl/CMakeFiles/sotl.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/atom.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/atom.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/device.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/device.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/domain.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/domain.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/global_definitions.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/global_definitions.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/kernel_list.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/kernel_list.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/ocl.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/ocl.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/ocl_kernels.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/ocl_kernels.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/openmp.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/openmp.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/profiling.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/profiling.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/seq.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/seq.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/shaders.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/shaders.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/sotl.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/sotl.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/util.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/util.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/vbo.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/vbo.c.o"
+  "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src/window.c" "/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/build/libsotl/CMakeFiles/sotl.dir/src/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "HAVE_LIBGL"
+  "HAVE_OMP"
+  "OCL_INCLUDE=\"/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/include\""
+  "PROGRAM_NAME=\"/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/kernel/physics.cl\""
+  "SHADERS_FILES_DIR=\"/net/cremi/acouture/Master1/PMG/projet-pmg/fichiers/libsotl/src\""
+  "USE_DOUBLE=0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../libsotl/include"
+  "../libsotl"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
